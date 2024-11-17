84d96bb6949cc53a88e73a028aea24297eb22911
oladipupot <oladipupotoheeb75@gmail.com>
2024-11-17 14:12:22 +0100

    secon commitment

--- atibyan-blog/src/App.jsx
+++ atibyan-blog/src/App.jsx
@@ -1,7 +1,22 @@
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Project from './pages/Project'
+import Signup from './pages/signup'
+import Signin from './pages/Signin'
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-700 ">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={ <Home /> } />
+      <Route path='/about' element={ <About /> } />
+      <Route path='/dashboard' element={ <Dashboard /> } />
+      <Route path='/project' element={ <Project /> } />
+      <Route path='/signup' element={ <Signup /> } />
+      <Route path='/signin' element={ <Signin /> } />
+    </Routes>
+    </BrowserRouter>
   )
 }
