4593cd43bde892ca17bbd07892cd0596fa5b7730
oladipupot <oladipupotoheeb75@gmail.com>
2024-11-29 22:52:31 +0100

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from "mongoose";
+
+
+const userschema = new mongoose.Schema({
+
+
+    username:{
+        type: String,
+        required: true,
+        unique: true,        
+    },
+
+
+
+    email:{
+        type: String,
+        required: true,
+        unique: true,    
+    }
+
+}, {timestamps: true})
+
+
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
